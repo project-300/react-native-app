f9519f03e34855d12344689923d0a914a25f084a
Charles McCarthy <charles@studybundles.com>
2019-11-28 23:12:37 +0000

    Driver Location Service

---
+++ src/services/driver-location.ts
@@ -0,0 +1,20 @@
+import { AsyncStorage } from 'react-native';
+
+interface Coords {
+	latitude: number;
+	longitude: number;
+}
+
+class DriverLocation {
+
+	public static setCurrentPosition = (c: Coords): Promise<void> => AsyncStorage.setItem('region', JSON.stringify(c));
+
+	public static getCurrentPosition = async (): Promise<Coords> => {
+		const pos: string | null = await AsyncStorage.getItem('region');
+
+		return pos ? JSON.parse(pos) as Coords : { latitude: 37.78825, longitude: -122.4324 };
+	}
+
+}
+
+export default DriverLocation;
