6b04625a074263c5e6342f3db726be636829a7b2
Charles McCarthy <charles@studybundles.com>
2020-01-25 21:07:09 +0000

    Updated Toast Notification UI

--- src/helpers/toastr.ts
+++ src/helpers/toastr.ts
@@ -1,4 +1,6 @@
 import Toast from 'react-native-root-toast';
+import { Dimensions, Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 type ToastTypeProps = {
 	backgroundColor?: string;
@@ -30,16 +32,30 @@ const info = (warning: string): void => {
 	showToast(warning, { });
 };
 
+const { width } = Dimensions.get('window');
+
 const showToast = (message: string, properties: ToastTypeProps): void => {
+	const paddingTop = Platform.OS === 'ios' ? getStatusBarHeight() + 10 : 20; // Avoid iOS status bar & notch
+
 	Toast.show(message, {
 		...properties,
 		duration: Toast.durations.LONG,
-		position: Toast.positions.BOTTOM,
+		position: Toast.positions.TOP,
 		shadow: true,
 		animation: true,
 		hideOnPress: true,
 		delay: 0,
-		opacity: 1
+		opacity: 1,
+		containerStyle: {
+			width, // Make it full width
+			borderRadius: 0, // Remove default rounded edges
+			padding: 20,
+			paddingTop,
+			marginTop: -21 // Remove default 20 pixels. Also removes extra 1 pixel sometimes displayed on Android
+		},
+		textStyle: {
+			fontWeight: 'bold'
+		}
 	});
 };
 
