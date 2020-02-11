c1684cc2ddd30528b82bc30d6d7efcfd32b919f3
Brian Keaveney <38507940+BrianKeaveney@users.noreply.github.com>
2020-02-11 23:31:25 +0000

    journey and user service added and environment template changed

--- environment/env.tpl.ts
+++ environment/env.tpl.ts
@@ -10,12 +10,25 @@ export const SERVER_WSS_URL: string = IS_OFFLINE ?
 	'{{ OFFLINE_SERVER_WSS_URL }}' :
 	'{{ LIVE_SERVER_WSS_URL }}';		// AWS Lambda WSS Stage URL
 
-export const AWS_CONFIG: AWS_COGNITO_CONFIG_TYPE = {
-	identityPoolId: '{{ IDENTITY_POOL_ID }}',						// AWS Cognito Identity Pool ID
-	region: '{{ REGION }}',											// AWS Region
-	userPoolId: '{{ USER_POOL_ID }}',								// AWS Cognito User Pool ID
-	userPoolWebClientId: '{{ USER_POOL_WEB_CLIENT_ID }}'			// AWS Cognito Client ID
+export const AWS_CONFIG = {
+	Auth: {
+		identityPoolId: '{{ IDENTITY_POOL_ID }}',						// AWS Cognito Identity Pool ID
+		region: '{{ REGION }}',											// AWS Region
+		userPoolId: '{{ USER_POOL_ID }}',								// AWS Cognito User Pool ID
+		userPoolWebClientId: '{{ USER_POOL_WEB_CLIENT_ID }}'			// AWS Cognito Client ID
+	  },
+	  API: {
+		endpoints: [
+		  {
+			name: '{{ API_NAME }}', 									// AWS API Name
+			endpoint: '{{ API_ENDPOINT }}',								// AWS API Endpoint
+			region: '{{ API_REGION }}'									// AWS API Region
+		  }
+		]
+	  }
 };
+
+export const ApiName = AWS_CONFIG.API.endpoints[0].name;
 
 export const S3_CONFIG: S3_CONFIG_TYPE_BRIEF = {
 	bucket: '{{ BUCKET }}',											// AWS S3 Bucket Name
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,11 @@
+import { ApiName } from './../../environment/env';
 import { API } from 'aws-amplify';
 import { User } from '@project-300/common-types';
-export class UserService {
-	private static readonly apiName: string = 'dev-P300-ApiGatewayRestApi';
 
-	public static async getUser(userId: string): Promise<User> {
-		return API.get(UserService.apiName, `/users/${userId}`, '');
-	}
+export class UserService {
+
+	public static getUser = async (userId: string): Promise<User> => API.get(ApiName, `/users/${userId}`, '');
+
+	public static updateUser = async (user: Partial<User>): Promise<void> => API.put(ApiName, '/users/', { body: user });
+
 }
